b2c46b4ecd5bfad1f949ec1752b6f1f687eeed85
ututehutuhetuh <86868292+ututehutuhetuh@users.noreply.github.com>
2021-12-13 23:05:13 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+class a{
+    public:
+        tring a;
+};
+class m{
+    public:
+        string m;
+};
+class o{
+    public:
+        string o;
+};
+class g{
+    public:
+        string g;
+};
+class u{
+    public:
+        string u;
+};
+class s{
+    public:
+        string s;
+};
+int main()
+{
+    a a;
+    m m;
+    o o;
+    g g;
+    u u;
+    s s;
+    a.a = "a";
+    m.m = "m";
+    o.o = "o";
+    g.g = "g";
+    u.u = "u";
+    s.s = "s";
+
+    cout << a.a << m.m << o.o << g.g << u.u << s.s << endl;
+    return 0;
+}
